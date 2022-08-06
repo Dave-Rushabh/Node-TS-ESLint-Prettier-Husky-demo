081bb2820cc9eaeb0f0393f10535cec8a7f08380
Rushabh Dave <rushabh.dave24@gmail.com>
2022-08-06 18:00:33 +0530

    ESLint & Prettier configured

--- DB/index.ts
+++ DB/index.ts
@@ -1,28 +1,28 @@
-import { Employee } from "../typeModels/index";
+import { Employee } from '../typeModels/index';
 
 export const employees: Employee[] = [
   {
     id: 1,
-    employeeName: "Bob harris",
+    employeeName: 'Bob harris',
     employeeAge: 35,
     employeePastExperience: [
       {
-        employeePreviousCompanyName: "ABC Infosolutions",
+        employeePreviousCompanyName: 'ABC Infosolutions',
         employeePreviousCompanyExperience: 12,
       },
       {
-        employeePreviousCompanyName: "Techno Giants",
+        employeePreviousCompanyName: 'Techno Giants',
         employeePreviousCompanyExperience: 13,
       },
     ],
   },
   {
     id: 2,
-    employeeName: "David Malan",
+    employeeName: 'David Malan',
     employeeAge: 24,
     employeePastExperience: [
       {
-        employeePreviousCompanyName: "Iris Softwares",
+        employeePreviousCompanyName: 'Iris Softwares',
         employeePreviousCompanyExperience: 0.5,
       },
     ],
--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 // collecting the express framework for writing APIs
-import express from "express";
-import { Request, Response } from "express";
-import { employees } from "../DB/index";
+import express from 'express';
+import { Request, Response } from 'express';
+import { employees } from '../DB/index';
 
 // express's instance is created with named 'app'
 const app = express();
@@ -10,21 +10,19 @@ const app = express();
 app.use(express.json());
 
 // GET API for all the fetching all employees' data
-app.get("/all-employees", (req: any, res: any) => {
+app.get('/all-employees', (req: any, res: any) => {
   res.send(employees);
 });
 
 // GET API for fetching employees' data based on ID
-app.get("/employee/:id", (req: any, res: any) => {
+app.get('/employee/:id', (req: any, res: any) => {
   const { id } = req.params;
-  const filteredEmployee = employees.filter(
-    (employee) => employee.id === parseInt(id)
-  );
+  const filteredEmployee = employees.filter((employee) => employee.id === parseInt(id));
   res.send(filteredEmployee);
 });
 
 //POST API for adding new employee is the list
-app.post("/add-employee", (req: Request, res: Response) => {
+app.post('/add-employee', (req: Request, res: Response) => {
   const employee = {
     id: employees.length + 1,
     employeeName: req.body.employeeName,
@@ -36,25 +34,21 @@ app.post("/add-employee", (req: Request, res: Response) => {
 });
 
 // PUT API for updating exisiting employee's details
-app.put("/update-employee/:id", (req: Request, res: Response) => {
+app.put('/update-employee/:id', (req: Request, res: Response) => {
   const { id } = req.params;
-  const filteredEmployee = employees.filter(
-    (employee) => employee.id === parseInt(id)
-  );
+  const filteredEmployee = employees.filter((employee) => employee.id === parseInt(id));
   filteredEmployee[0].employeeName = req.body.employeeName;
   res.send(filteredEmployee);
 });
 
 //DELETE API for deleting the employee's details
-app.delete("/delete-employee/:id", (req: Request, res: Response) => {
+app.delete('/delete-employee/:id', (req: Request, res: Response) => {
   const { id } = req.params;
-  const filteredEmployees = employees.filter(
-    (employee) => employee.id !== parseInt(id)
-  );
+  const filteredEmployees = employees.filter((employee) => employee.id !== parseInt(id));
   res.send(filteredEmployees);
 });
 
 // express server started on port:3000
 app.listen(3000, () => {
-  console.log("The application is listening on port 3000!");
+  console.log('The application is listening on port 3000!');
 });
